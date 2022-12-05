e67f36ed9174aa5d81233c649689b47cbf301fbc
c-basalt <117849907+c-basalt@users.noreply.github.com>
2022-12-04 20:29:56 -0500

    update

--- fc2_record.py
+++ fc2_record.py
@@ -3,9 +3,13 @@ import json
 import time
 import asyncio
 import subprocess
+import signal
 import urllib.parse
 import sys
 import os
+import argparse
+from multiprocessing import Process
+import traceback
 
 import aiohttp
 from aiohttp import web
@@ -21,14 +25,14 @@ class ProxyServer:
             web.get('/m3u8', self.m3u8),
             web.get('/file', self.file),
         ])
+        self.runner = web.AppRunner(self.app)
 
     async def start(self):
-        runner = web.AppRunner(self.app)
-        await runner.setup()
+        await self.runner.setup()
         addr, port = ADDR, PORT
         for i in range(10000):
             try:
-                site = web.TCPSite(runner, addr, port)
+                site = web.TCPSite(self.runner, addr, port)
                 await site.start()
                 print('server started at %s:%s' % (addr, port))
                 return addr, port
@@ -65,13 +69,21 @@ def test_ffmpeg():
         return False
 
 class FC2Handler:
-    def __init__(self, channel_id):
+    def __init__(self, channel_id, out_dir):
         self.channel_id = channel_id
         self.server = ProxyServer(self)
-        self.basename = 'fc2-record/%s-%.0f' % (channel_id, time.time())
+        self.out_dir = out_dir
+        self.basename = os.path.join(out_dir, '%s-%.0f' % (channel_id, time.time()))
         self.ffmpeg_future = None
+        self.ffmpeg_proc = None
         self.log_queue = asyncio.Queue()
         self.chat_queue = asyncio.Queue()
+        self._ws_count = 0
+
+    @property
+    def ws_count(self):
+        self._ws_count += 1
+        return self._ws_count
 
     async def logging_worker(self):
         while True:
@@ -79,7 +91,7 @@ class FC2Handler:
                 (msg, print_log) = await self.log_queue.get()
                 log_str = '[%s] %s\n' % (time.asctime(), msg)
                 if print_log:
-                    print(log_str, end='')
+                    print(log_str[:600], end='')
                 f.write(log_str)
 
     async def chat_worker(self):
@@ -90,9 +102,9 @@ class FC2Handler:
     
     async def ffmpeg_worker(self):
         with open(self.basename + '.ts.log', 'at', encoding='utf-8') as f:
-            cmd = 'ffmpeg -hide_banner -i "http://%s:%s/m3u8" -c copy %s.ts' % (self.addr, self.port, self.basename)
-            p = await asyncio.create_subprocess_shell(cmd, stdout=f, stderr=f)
-            await p.wait()
+            cmd = ['ffmpeg', '-hide_banner', '-i', 'http://%s:%s/m3u8'% (self.addr,self.port), '-c', 'copy', '%s.ts'%self.basename]
+            self.ffmpeg_proc = await asyncio.create_subprocess_exec(*cmd, stdout=f, stderr=f)
+            await self.ffmpeg_proc.wait()
 
     def log(self, msg, print_log=False):
         self.log_queue.put_nowait((msg, print_log))
@@ -105,13 +117,13 @@ class FC2Handler:
 
     async def get(self, url):
         async with self.session.get(url) as r:
-            # print(url, r.status)
-            if r.status != 200:
-                self.log('%d %s\n%s' % (r.status, r.url, (await r.data())), print_log=True)
-                if r.status == 403:
-                    await websocket.send_json({"name":"get_hls_information","arguments":{},"id":1})
             data = await r.read()
-            self.log(url)
+            if r.status == 200:
+                self.log(url)
+            else:
+                self.log('%d %s\n%s' % (r.status, r.url, data), print_log=True)
+                if self.websocket and r.status == 403:
+                    await self.websocket.send_json({"name":"get_hls_information","arguments":{},"id":self.ws_count})
             return data
 
     async def get_m3u8(self):
@@ -149,35 +161,34 @@ class FC2Handler:
         return '%s?control_token=%s' % (data['url'], data['control_token'])
 
     async def heartbeat(self, websocket):
-        count = 2
         while True:
-            await websocket.send_json({"name":"heartbeat","arguments":{},"id":count})
+            await websocket.send_json({"name":"heartbeat","arguments":{},"id":self.ws_count})
             await asyncio.sleep(30)
-            count += 1
 
     async def main(self):
         headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
         self.session = aiohttp.ClientSession(headers=headers)
         self.addr, self.port = await self.server.start()
         url = await self.get_control_server()
+        os.makedirs(self.out_dir, exist_ok=True)
         async with self.session.ws_connect(url) as websocket:
             self.websocket = websocket
-            await websocket.send_json({"name":"get_hls_information","arguments":{},"id":1})
+            await websocket.send_json({"name":"get_hls_information","arguments":{},"id":self.ws_count})
             self.heartbeat_future = asyncio.ensure_future(self.heartbeat(websocket))
             self.logger_future = asyncio.ensure_future(self.logging_worker())
             self.chat_future = asyncio.ensure_future(self.chat_worker())
             async for message in websocket:
-                try:
-                    await self.handle_ws_message(message)
-                except KeyboardInterrupt:
-                    break
+                await self.handle_ws_message(message)
+            self.websocket = None
+            print('websocket disconnect')
             self.heartbeat_future.cancel()
-            self.ffmpeg_future.cancel()
-        self.logger_future.cancel()
-        self.chat_future.cancel()
-        await self.ffmpeg_future
-        await self.logger_future
-        await self.chat_future
+            if self.ffmpeg_future:
+                self.ffmpeg_proc.send_signal(signal.SIGTERM)
+                try:
+                    await asyncio.wait_for(self.ffmpeg_future, 3)
+                except asyncio.TimeoutError:
+                    pass
+            await asyncio.sleep(0.1)
 
     async def handle_ws_message(self, message):
         if message.type == aiohttp.WSMsgType.TEXT:
@@ -188,7 +199,7 @@ class FC2Handler:
                     if i['mode'] > max_mode and i['mode'] < 90:
                         self.m3u8_url = i['url']
                         max_mode = i['mode']
-                self.log('start recording: %s' % self.m3u8_url, print_log=True)
+                self.log('start recording %s: %s' % (max_mode, self.m3u8_url), print_log=True)
                 if not self.ffmpeg_future:
                     self.ffmpeg_future = asyncio.ensure_future(self.ffmpeg_worker())
             elif data.get('name', None) in [
@@ -205,15 +216,34 @@ class FC2Handler:
             elif data.get('name', None) == 'comment':
                 self.chat_queue.put_nowait(data['arguments'])
             else:
-                self.log('%s %s' % (data.keys(), str(data)[:300]), print_log=True)
+                self.log('%s %s' % (data.keys(), str(data)), print_log=True)
             return data.get('name', None)
 
-if __name__ == '__main__':
+def wrapper(channel_id, out_dir):
+    try:
+        asyncio.get_event_loop().run_until_complete(FC2Handler(channel_id, out_dir).main())
+    except Exception:
+        traceback.print_exc()
+
+def main():
+    parser = argparse.ArgumentParser(description='Dump fc2 live stream with ffmpeg', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('channel_id', type=int, help='id number in url')
+    parser.add_argument('--loop_interval', type=float, metavar='N',help='number of seconds between tries, exit after single run if not specified')
+    parser.add_argument('--out_dir', type=str, default='fc2-record', help='dir for output')
+    args = parser.parse_args()
     if not test_ffmpeg():
         print('Please make sure ffmpeg is installed and added to PATH')
         exit(1)
-    if len(sys.argv) != 2:
-        print(sys.argv[0], '<channel_id>')
+    os.makedirs(args.out_dir, exist_ok=True)
+    if args.loop_interval:
+        while True:
+            p = Process(target=wrapper, args=(args.channel_id, args.out_dir,))
+            p.start()
+            p.join()
+            time.sleep(args.loop_interval)
     else:
-        os.makedirs('fc2-record', exist_ok=True)
-        asyncio.get_event_loop().run_until_complete(FC2Handler(sys.argv[1]).main())
+        asyncio.get_event_loop().run_until_complete(FC2Handler(args.channel_id, args.out_dir).main())
+
+
+if __name__ == '__main__':
+    main()
